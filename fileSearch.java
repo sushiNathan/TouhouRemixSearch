357d95f025dbc216e74bc82a69a1004017690f5a
sushiNathan <nathanmangaoil49@gmail.com>
2015-11-29 16:29:35 -0500

    Completed fileSearch.java
    The program now creates new text files with the all the songs in the current directory and searches for and prints out songs based on a given tag.

--- fileSearch.java
+++ fileSearch.java
@@ -1,21 +1,40 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 public class fileSearch {
 	public static void main(String[] args)throws IOException{
-		if (args[0].equals("new")){
+		search();
+	}
+	public static void search()throws IOException{
+		System.out.println("Enter 'new' to create a new text file");
+		System.out.println("Enter 'search' to search for songs");
+		System.out.println("Enter 'exit' to exit the program");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		if (input.equals("new")){
 			newFile();
 		}
-		
-		else if (args[0].equals("search")){
-			String tag = args[1];
+		else if (input.equals("search")){
+			System.out.println("Enter tag: ");
+			String tag = sc.next();
+			tagSearch(tag);
 		}
+		else if (input.equals("exit")){
+			System.exit(0);
+		}
+		else{
+			System.out.println("Invalid input");
+			search();
+		}
+		sc.close();
 	}
 	
 	public static void newFile() throws IOException{
-		System.out.println(System.getProperty("user.dir")); //Displays the current directory
+		System.out.println("Adding the following songs");
 		File f = new File("."); // current directory
 		File[] files = f.listFiles(); //Creates an array of the files of the current directory
 		FileWriter text = new FileWriter("songs.txt");
@@ -30,10 +49,30 @@ public class fileSearch {
 			}
 		}
 		text.close();
+		System.out.println("Text file created");
+		search();
 	}
 	
 	public static void tagSearch(String tag) throws IOException{
-		
+		System.out.println("Displaying songs");
+		FileReader fr = new FileReader( "songs.txt" );
+		BufferedReader br = new BufferedReader( fr );
+		String stringRead = br.readLine( );
+		while( stringRead != null ){ // test for the end of the file
+		// print the line read
+			try{
+				String endOfName = stringRead.substring(stringRead.lastIndexOf(".")+5, stringRead.length());
+			
+				if(endOfName.equals(tag)){//Print the song name if its tag is the same as the inputted tag
+					System.out.println(stringRead.substring(0, stringRead.lastIndexOf(".")));
+				}
+				stringRead = br.readLine( ); // read next line
+			}
+			catch(IndexOutOfBoundsException e){
+				stringRead = br.readLine( ); // read next line
+			}
+		}
+		br.close();
+		search();
 	}
-
 }
